623cb9688aab089994542a0c3f27173bd7578d00
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-13 22:15:04 +0530

    Connect with mongodb

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,28 @@
-import express from 'express';
+require('dotenv').config();
+import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// Testing api
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: 'API is working',
+  });
+});
+
+// Unknown route
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found.`) as any;
+  err.statusCode = 404;
+  next(err);
+});
