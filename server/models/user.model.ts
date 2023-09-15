17b208e93e2ad1031c8baea00d93bee54bbf8e09
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-15 12:17:05 +0530

    feat(schema): create user schema
    - Added a new Mongoose schema for user data.
    - Defined fields for username, email, and password.
    - Set validation rules for email format.
    - Hash password before saving
    - Compare password

---
+++ server/models/user.model.ts
@@ -0,0 +1,77 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userShema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter you email'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter your email'],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: 'Please enter a valid email',
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your password'],
+      minlength: [6, 'Password must be at least 6 characters'],
+      select: false, // Password field won't be selected by default
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: 'user',
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [{ courseId: String }],
+  },
+  { timestamps: true }
+);
+
+// Hash Password before saving
+userShema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+userShema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model('User', userShema);
+export default userModel;
