0d23683e9295aa84d330059300bcb957796aea5c
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-18 12:07:16 +0530

    feat(auth): add update password

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -8,6 +8,7 @@ import {
   registrationUser,
   socialAuth,
   updateAccessToken,
+  updatePassword,
   updateUserInfo,
 } from '../controllers/user.controller';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
@@ -23,5 +24,6 @@ userRouter.get('/refresh', updateAccessToken);
 userRouter.get('/me', isAuthenticated, getUserInfo);
 userRouter.post('/social-auth', socialAuth);
 userRouter.put('/update-user-info', isAuthenticated, updateUserInfo);
+userRouter.put('/update-user-password', isAuthenticated, updatePassword);
 
 export default userRouter;
