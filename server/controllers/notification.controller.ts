06610ea6142c1d685feddb5213001859c15febd0
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-23 00:38:01 +0530

    feat(admin): delete read notifications after 30days

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Response, Request } from 'express';
+import cron from 'node-cron';
 
 import NotificationModel from '../models/notification.model';
 import { CatchAsyncError } from '../middleware/catchAsyncErrors';
@@ -55,3 +56,12 @@ export const updateNotification = CatchAsyncError(
     }
   }
 );
+
+// delete read notification after 30days
+cron.schedule('0 0 0 * * *', async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await NotificationModel.deleteMany({
+    status: 'read',
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+});
