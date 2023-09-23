861d4bfad9e7d08f305060fcccf8b9c53e47cd2c
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-23 11:18:16 +0530

    feat(admin): delete user

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -293,7 +293,6 @@ export const socialAuth = CatchAsyncError(
 );
 
 //  Update user info
-
 interface IUpdateUserInfo {
   name?: string;
   email?: string;
@@ -448,6 +447,31 @@ export const updateUserRole = CatchAsyncError(
       const { id, role } = req.body;
 
       updateUserRoleService(res, id, role);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// update user role - only for admin
+export const deleteUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      const user = await userModel.findById(id);
+
+      if (!user) {
+        return next(new ErrorHandler('User not found', 404));
+      }
+
+      await user.deleteOne({ id });
+      await redis.del(id);
+
+      res.status(200).json({
+        success: true,
+        message: 'User deleted successfully.',
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
