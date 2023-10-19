1e740ee75ba8a1208fbdd9b5bbede7f8afd344fc
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-10-19 11:13:50 +0530

    feat(client): render dynamic data in the hero section

--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -1,30 +1,28 @@
 import Image from 'next/image';
 
+import { useGetHeroDataQuery } from '@/redux/features/layout/layoutApi';
+
 const Hero = () => {
+  const { data, refetch, isLoading } = useGetHeroDataQuery('Banner', {});
+
   return (
     <>
-      <div className="mt-24 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
+      <div className="mt-12 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8 h-full">
         <div className="space-y-4 flex-1 sm:text-center lg:text-left">
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
             A world of learning from{' '}
             <span className="text-[crimson]">₹449</span>
           </p>
           <h1 className="dark:text-white text-black font-bold text-4xl xl:text-5xl">
-            Accelerate Your <br />
-            <span className="dark:text-[#37a39a] text-[crimson]">
-              {' '}
-              Learning Journey
-            </span>
+            {data?.layout?.banner?.title}
           </h1>
           <p className="dark:text-gray-300 text-black max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
-            Explore a world of knowledge with our learnly eLearning platform.{' '}
-            <br /> Join educators and learners worldwide on a journey of
-            discovery.
+            {data?.layout?.banner?.subTitle}
           </p>
           <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
             <a
               href="javascript:void(0)"
-              className="px-7 py-3 w-full dark:bg-white bg-[crimson] text-white dark:text-gray-800 text-center rounded-md shadow-md block sm:w-auto"
+              className="px-7 py-3 w-full bg-[crimson] text-white text-center rounded-md shadow-md block sm:w-auto"
             >
               Explore courses
             </a>
@@ -37,17 +35,18 @@ const Hero = () => {
           </div>
         </div>
         <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
-          {/* <Image
-            src="/assets/learnly.jpeg"
+          <Image
+            src={data?.layout?.banner?.image?.url}
             className="w-full mx-auto sm:w-10/12 lg:w-full"
             alt="Hero"
-            width={1000}
-            height={50}
-          /> */}
-          <img
+            width={400}
+            height={400}
+          />
+          {/* <img
             src="https://i.postimg.cc/HxHyt53c/undraw-heatmap-uyye.png"
             className="w-full mx-auto sm:w-10/12  lg:w-full"
-          />
+          /> */}
+          {/* data?.layout?.banner?.image?.url */}
         </div>
       </div>
     </>
