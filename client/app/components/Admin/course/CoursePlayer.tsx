e0097abbfaac1f06e8077cf1140be9ecd1d2bcdb
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-10-25 15:33:21 +0530

    fix: fix course player width

--- client/app/components/Admin/course/CoursePlayer.tsx
+++ client/app/components/Admin/course/CoursePlayer.tsx
@@ -21,13 +21,15 @@ const CoursePlayer: React.FC<Props> = ({ title, videoUrl }) => {
   }, [videoUrl]);
 
   return (
-    <div style={{ paddingTop: '41%', position: 'relative' }}>
+    <div
+      style={{ paddingTop: '45%', position: 'relative', overflow: 'hidden' }}
+    >
       {videoData.otp && videoData.playbackInfo !== '' && (
         <iframe
           src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData?.playbackInfo}&player=QdBu8SBfu1N967gf`}
           style={{
             border: 0,
-            width: '90%',
+            width: '100%',
             height: '100%',
             position: 'absolute',
             top: 0,
--- client/app/components/course/CourseContentMedia.tsx
+++ client/app/components/course/CourseContentMedia.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CoursePlayer from '../Admin/course/CoursePlayer';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
@@ -26,10 +26,12 @@ interface CourseData {
   videoLength: number;
   videoSection: string;
   videoUrl: string;
-  links: {
-    title: string;
-    url: string;
-  };
+  links: [
+    {
+      title: string;
+      url: string;
+    }
+  ];
   questions: CommentData[];
   suggestion: string;
 }
@@ -48,23 +50,25 @@ const CourseContentMedia: React.FC<CourseContentMediaProps> = ({
   setActiveVideo,
 }) => {
   console.log(courseData);
+  const [activeBar, setActiveBar] = useState(0);
+
   return (
     <div className="w-[95%] 800px:w-[86%] py-4 m-auto">
       <CoursePlayer
         title={courseData[activeVideo]?.title}
         videoUrl={courseData[activeVideo]?.videoUrl}
       />
-      <div className="w-full flex items-center justify-between my-3 relative">
+      <div className="w-full flex items-center justify-between my-3">
         <div
           className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 px-4 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 !min-h-[40px] !py-[unset] ${
             activeVideo === 0 && '!cursor-no-drop opacity-[0.8]'
           }`}
           onClick={() => (activeVideo === 0 ? 0 : activeVideo - 1)}
         >
-          <AiOutlineArrowLeft className="mr-2" /> Prev
+          <AiOutlineArrowLeft className="mr-2" /> Prev Lecture
         </div>
         <div
-          className={`text-white absolute top-0 right-20 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 px-4 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 !min-h-[40px] !py-[unset] ${
+          className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 px-4 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 !min-h-[40px] !py-[unset] ${
             courseData.length - 1 === activeVideo &&
             '!cursor-no-drop opacity-[0.8]'
           }`}
@@ -76,8 +80,50 @@ const CourseContentMedia: React.FC<CourseContentMediaProps> = ({
             )
           }
         >
-          Next <AiOutlineArrowRight className="ml-2" />
+          Next Lecture
+          <AiOutlineArrowRight className="ml-2" />
         </div>
+      </div>
+      <h1 className="pt-2 text-[25px] font-[600] dark:text-white">
+        {courseData[activeVideo]?.title}
+      </h1>
+      <br />
+      <div className="border">
+        <div className="p-4 flex items-center justify-between bg-slate-500 bg-opacity-20 backdrop-blur shadow-[bg-slate-700] rounded shadow-inner">
+          {['Overview', 'Resourses', 'Q & A', 'Reviews'].map((text, index) => (
+            <h5
+              key={index}
+              className={`800px:text-[20px] cursor-pointer ${
+                activeBar === index
+                  ? 'text-blue-700'
+                  : 'dark:text-white text-black'
+              }`}
+              onClick={() => setActiveBar(index)}
+            >
+              {text}
+            </h5>
+          ))}
+        </div>
+        <br />
+        {activeBar === 0 && (
+          <p className="text-[18px] p-4 whitespace-pre-line mb-3 dark:text-white text-black">
+            {courseData[activeVideo]?.description}
+          </p>
+        )}
+        {activeBar === 1 && (
+          <div>
+            {courseData[activeVideo]?.links.map((link, index) => (
+              <div className="mb-5 p-4" key={index}>
+                <h2 className="800px:text-[20px] 800px:inline-block font-sans dark:text-white">
+                  {link.title && link.title + ' : '}
+                </h2>
+                <a className="inline-block mx-2 text-blue-500" href={link.url}>
+                  {link.url}
+                </a>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
