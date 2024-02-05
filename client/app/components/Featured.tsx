d71e64c418665a1e6c4522758d1ee3fc96d5d7c5
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-05 22:54:36 +0530

    feat(ui): update featured section

--- client/app/components/Featured.tsx
+++ client/app/components/Featured.tsx
@@ -29,7 +29,7 @@ const Featured = () => {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
+              <h2 className="text-base font-semibold leading-7 text-[crimson]">
                 Discover Learnly
               </h2>
               <p className="mt-2 text-3xl dark:text-white font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -44,7 +44,7 @@ const Featured = () => {
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900 dark:text-white">
                       {
-                        <feature.icon className="inline-block mr-2 text-indigo-600" />
+                        <feature.icon className="inline-block mr-2 text-[crimson]" />
                       }
                       {feature.name}
                     </dt>
