558e7ed3e590e7a267405e8de8a20f30c45a5a26
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-06 11:05:09 +0530

    fix(server): update dotenv imports

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
